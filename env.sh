7868e1d57db1588e987b945dc1e308418a9a01a4
Sam Crang <sam.crang@gmail.com>
2013-07-14 21:09:43 +0100

    pull name/email into variables

--- env.sh
+++ env.sh
@@ -1,4 +1,7 @@
-export GIT_COMMITTER_NAME="Sam Crang"
-export GIT_COMMITTER_EMAIL="sam.crang@gmail.com"
-export GIT_AUTHOR_NAME="Sam Crang"
-export GIT_AUTHOR_EMAIL="sam.crang@gmail.com"
+export NAME="Sam Crang"
+export EMAIL="sam.crang@gmail.com"
+
+export GIT_COMMITTER_NAME=$NAME
+export GIT_COMMITTER_EMAIL=$EMAIL
+export GIT_AUTHOR_NAME=$NAME
+export GIT_AUTHOR_EMAIL=$EMAIL
