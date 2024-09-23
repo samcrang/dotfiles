0a47a6bcda02822e9cec35095857be375498ed78
Sam Crang <sam.crang@gmail.com>
2024-09-23 15:32:09 +0100

    Configure nvim-cmp

---
+++ home/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,12 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<CR>"] = cmp.config.disable,
+      ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    })
+  end,
+}
