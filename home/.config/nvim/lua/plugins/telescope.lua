7e1e154546f6c0fbc21e0925cfb3236ddfe5bf78
Sam Crang <sam.crang@gmail.com>
2024-09-23 16:15:15 +0100

    Add telescope keymaps that feel more natural

---
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      mappings = {
+        i = {
+          ["<C-j>"] = "move_selection_next",
+          ["<C-k>"] = "move_selection_previous",
+        },
+      },
+    },
+  },
+}
