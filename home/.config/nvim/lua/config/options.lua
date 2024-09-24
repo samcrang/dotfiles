e94f094d2cbbc7f50cb5cf890d5c4177736194fe
Sam Crang <sam.crang@gmail.com>
2024-09-24 18:08:41 +0100

    Disable relative line numbers

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -2,3 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_ruby_lsp = "solargraph"
+
+vim.opt.relativenumber = false
