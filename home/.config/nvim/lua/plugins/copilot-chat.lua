647f86265f7a119c9a09d9dae6ea6ba6b333fcbc
Sam Crang <sam.crang@gmail.com>
2024-09-27 23:14:07 +0100

    Avoid using mapping that interferes with pane navigation

---
+++ home/.config/nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,12 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  opts = {
+    auto_insert_mode = false,
+    mappings = {
+      reset = {
+        normal = "<C-q>",
+        insert = "<C-q>",
+      },
+    },
+  },
+}
