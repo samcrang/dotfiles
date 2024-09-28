284b12fa4a84ebfe2388a6d75989ab49bdc066d5
Sam Crang <sam.crang@gmail.com>
2024-09-28 13:07:41 +0100

    Remove unused keymap
    I don't use this so may as well leave it clear for something else.

--- home/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ home/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -12,6 +12,5 @@ return {
     { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
     { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
     { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
   },
 }
